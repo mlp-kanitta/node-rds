a96fc9a5740d77c05066168847b944cfe3e21bec
Kanitta Moonlapong <kanitta.m@kbtg.tech>
2024-02-11 16:02:12 +0700

    Init #1

---
+++ dbseed.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: "houserentdb.cbc8mqgu6oox.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "0YZCnztnoKnOQjNwCOGn"
+});
+
+
+
+con.connect(function(err) {
+    if (err) throw err;
+
+    con.query('CREATE DATABASE IF NOT EXISTS main;');
+    con.query('USE main;');
+    con.query('CREATE TABLE IF NOT EXISTS users(id int NOT NULL AUTO_INCREMENT, username varchar(30), '+
+    'email varchar(255), age int, PRIMARY KEY(id));', function(error, result, fields) {
+        console.log(result);
+    });
+    con.end();
+});
